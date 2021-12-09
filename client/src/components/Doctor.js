7a4812a0469016148521998ada9d3e5a66293096
John Leosco <jrleosco@gmail.com>
2021-12-08 17:19:14 -0700

    edit toggle

--- client/src/components/Doctor.js
+++ client/src/components/Doctor.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import DoctorForm from "./DoctorForm";
 import {Button} from 'semantic-ui-react';
 
-
-
 const Doctor = (props) => {
+  const [editToggle, setEditToggle] = useState(false)
   const {id, deleteDoctor, name, updateDoctor} = props
-return (
+
+  if (editToggle) {
+    return (
+    <div>
+      <p>Name:{props.name}</p>
+      <DoctorForm id={id} name={name}updateDoctor={updateDoctor}/>
+      <Button onClick={() => setEditToggle(!editToggle)}>Hide</Button>
+      <Button onClick={()=> deleteDoctor(id)}>Delete</Button>
+    </div>
+  )} else {
+  return(
   <div>
-    <p>Name:{props.name}</p>
-    <DoctorForm id={id} name={name}updateDoctor={updateDoctor}/>
+  <p>Name:{props.name}</p>
+    <Button onClick={() => setEditToggle(!editToggle)}>Edit</Button>
     <Button onClick={()=> deleteDoctor(id)}>Delete</Button>
   </div>
-);
+)}
 };
 
 export default Doctor;
