e21a06e85b32d6702b92bb2236ac8d15e965121b
John Leosco <jrleosco@gmail.com>
2021-12-09 14:45:31 -0700

    time formatted

--- client/src/components/Appointment.js
+++ client/src/components/Appointment.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import moment from 'moment'
 
 const Appointment = (props) => {
   
@@ -6,7 +7,7 @@ const Appointment = (props) => {
     <div style={{border: "1px solid black", margin: '5px'}}>
       <h3>Dr: {props.doctor}</h3>
       <h3>Patient: {props.patient}</h3>
-      <h3>Appointment: {props.session}</h3>
+      <h3>Appointment: {moment(`${props.session}`).format('MMMM Do YYYY')}</h3>
     </div>
   );
 };
